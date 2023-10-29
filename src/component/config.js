3c1cb87645cc815e845536fc137bc21c1ab4a484
Eomdapin <qlsekdja2@gmail.com>
2023-10-29 22:01:51 +0900

    last

--- src/component/config.js
+++ src/component/config.js
@@ -1,3 +1,4 @@
-const API_URL = 'https://pixabay.com/api/?key=40348210-04f2daa0a80d3aabe9e8e9403&q=keyBoard&image_type=photo&category=computer&min_width=400&min_height=400&page=';
+const API_KEY = process.env.REACT_APP_API;
+const API_URL = `https://pixabay.com/api/?key=${API_KEY}&q=keyBoard&image_type=photo&category=computer&min_width=400&min_height=400&page=`;
 
 export { API_URL };
