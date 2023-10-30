a2689f5a283a0d9a6198420cff635b4861894db3
eomdapin <qlsekdja2@gmail.com>
2023-10-30 16:55:59 +0900

    Carousel add

--- src/component/MainBanner.js
+++ src/component/MainBanner.js
@@ -1,11 +1,23 @@
 import React from 'react';
 import Content from './Content.js';
+import { Carousel } from 'antd';
 
 function MainBanner() {
   return (
     <>
       <div id='main_banner'>
-        <div>키보드 서퍼에 오신 것을 환영합니다.</div>
+        <div className="desc">키보드 서퍼에 오신 것을 환영합니다.</div>
+        <Carousel autoplay>
+          <div>
+            <h3 className="bg_01"></h3>
+          </div>
+          <div>
+            <h3 className="bg_02"></h3>
+          </div>
+          <div>
+            <h3 className="bg_03"></h3>
+          </div>
+        </Carousel>
       </div>
       <Content />
     </>
