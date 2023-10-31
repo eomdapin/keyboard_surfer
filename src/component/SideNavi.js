b28eeeddc8b46e83935b5ca752579c28ff67eb13
eomdapin <qlsekdja2@gmail.com>
2023-10-31 11:24:10 +0900

    11:24

--- src/component/SideNavi.js
+++ src/component/SideNavi.js
@@ -9,19 +9,13 @@ function SideNavi() {
     product: '상품목록',
   }
   const pathSub = {
-    sub_1: 'product/sub_1',
-    sub_2: 'product/sub_2',
-    sub_3: 'product/sub_3',
-    sub_4: 'product/sub_4',
-    sub_5: 'product/sub_5',
-    notice: 'notice/notice',
-    event: 'notice/event',
+    notice: ['notice/notice', 'notice/event'],
+    product: ['product/sub_1', 'product/sub_2', 'product/sub_3', 'product/sub_4', 'product/sub_5',],
   };
   const pathName = {
     notice: ['공지사항', '이벤트'],
     product: ['풀배열 키보드', '텐키리스 키보드', '66Key 키보드', '마우스 패드', '악세서리'],
   };
-
 
   return (
     <div id="side_nav">
@@ -30,7 +24,7 @@ function SideNavi() {
         return (
           <React.Fragment key={index}>
             <div className="side_btn">
-              <a href={path.concat(pathSub[sub_2])}>{value}</a>
+              <a href={path.concat(pathSub[sub_1][index])}>{value}</a>
             </div>
           </React.Fragment>
         );
